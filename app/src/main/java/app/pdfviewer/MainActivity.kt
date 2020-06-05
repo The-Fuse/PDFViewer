74119cc97845f4995f1fd7cfcff88f74393d6280
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-06-05 16:07:36 +0530

    Notification Added to each button press.😁😉😉😋😋

--- app/src/main/java/app/pdfviewer/MainActivity.kt
+++ app/src/main/java/app/pdfviewer/MainActivity.kt
@@ -1,7 +1,14 @@
 package app.pdfviewer
 
 import android.app.Activity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
@@ -18,9 +25,16 @@ class MainActivity : AppCompatActivity() {
     companion object{
         private val PICK_PDF_CODE = 1000
     }
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "app.pdfviewer"
+    private val description = "PDF notification"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         verifyuserloggedin()
         var url = "https://firebasestorage.googleapis.com/v0/b/webappquery.appspot.com/o/RohitKumar.pdf?alt=media&token=f2cdfca5-4e30-4b5d-810b-97750f1d09f2"
         Dexter.withActivity(this)
@@ -38,17 +52,63 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         btn_view_assets.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId,description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor= Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                    .setContentTitle("PDF Viewer")
+                    .setContentText("You just clicked on View from assets")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher_round))
+            }
+            notificationManager.notify(123,builder.build())
+
+
             val intent = Intent(this,ViewActivity::class.java)
             intent.putExtra("ViewType","assets")
             startActivity(intent)
         }
         btn_view_storage.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId,description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor= Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                    .setContentTitle("PDF Viewer")
+                    .setContentText("You just clicked on View from System Storage")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher_round))
+            }
+            notificationManager.notify(789,builder.build())
+
             val pdfintent = Intent(Intent.ACTION_GET_CONTENT)
             pdfintent.type = "application/pdf"
             pdfintent.addCategory(Intent.CATEGORY_OPENABLE)
             startActivityForResult(Intent.createChooser(pdfintent,"Select PDF"), PICK_PDF_CODE)
         }
         btn_view_internet.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId,description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor= Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                    .setContentTitle("PDF Viewer")
+                    .setContentText("You just clicked on View from Internet")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher_round))
+            }
+            notificationManager.notify(456,builder.build())
+
             val intent = Intent(this,ViewActivity::class.java)
             intent.putExtra("ViewType","internet")
             startActivity(intent)
